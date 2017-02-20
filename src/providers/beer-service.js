3484788e4c961e540c2c631f3df377677f15b844
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2017-02-20 12:24:43 +0100

    refactoring parsemodel function

--- src/providers/beer-service.js
+++ src/providers/beer-service.js
@@ -5,27 +5,7 @@ define(['ojs/ojcore'], function (oj) {
 	  var Beer = oj.Model.extend({
 		urlRoot: this.resourceUrl,
 		idAttribute: "name",
-		parse: function (response) {
-		  var tags = [];
-		  var responseName = response.name;
-		  var name = response.name.replace(/ /g, '+');
-		  var url = "http://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=dc6zaTOxFJmzC";
-		  var xhr = new XMLHttpRequest();
-		  xhr.onload = function () {
-			if (xhr.readyState == 4 && xhr.status == 200) {
-			  var response = JSON.parse(xhr.responseText);
-			  var data = response.data;
-			  tags.push({
-				name: responseName,
-				image: data[0].images.downsized.url
-			  });
-			  console.log(name + ": " + data[0].images.downsized.url);
-			}
-		  };
-		  xhr.open('GET', url, false);
-		  xhr.send();
-		  return tags;
-		}
+		parse: parseModel
 	  });
 	  return new Beer();
 	},
@@ -37,5 +17,26 @@ define(['ojs/ojcore'], function (oj) {
 	  return new Beers();
 	}
   };
+  function parseModel(response) {
+	var tags = [];
+	var responseName = response.name;
+	var name = response.name.replace(/ /g, '+');
+	var url = "http://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=dc6zaTOxFJmzC";
+	var xhr = new XMLHttpRequest();
+	xhr.onload = function () {
+	  if (xhr.readyState == 4 && xhr.status == 200) {
+		var response = JSON.parse(xhr.responseText);
+		var data = response.data;
+		tags.push({
+		  name: responseName,
+		  image: data[0].images.downsized.url
+		});
+		console.log(name + ": " + data[0].images.downsized.url);
+	  }
+	};
+	xhr.open('GET', url, false);
+	xhr.send();
+	return tags;
+  }
   return BeerService;
 });
